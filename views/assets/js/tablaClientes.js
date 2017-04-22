36179817511b95ed270d9d055f35c8d08e2c08ca
Juanganzo69 <juan1910@live.com.mx>
2017-04-22 17:19:42 -0500

    Iconos y más responsivo
    Implementación de agregarCliente e íconos

--- views/assets/js/tablaClientes.js
+++ views/assets/js/tablaClientes.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
     var table = $('#tdClientes').DataTable( {
         	lengthChange: true,
+        	responsive: true,
            	dom: 'Bfrtipl',
 		    buttons: [
+		        {
+		        	text:'<i class="fa fa-plus-circle" aria-hidden="true"></i>',
+		        	className: 'btn color btn-success',
+		        	titleAttr: 'Agregar Cliente',
+		        	action: function( e, dt, node, config ){
+		        		location.href="index.php?action=agregarCliente";
+		        	}
+		        },
 		    	{
+		    		text:'<i class="fa fa-file-pdf-o" aria-hidden="true"></i>',
 		            extend: 'pdfHtml5',
+		            titleAttr: 'Importa a PDF',
 		            message: 'listado de todos los atributos de los clientes',
 		            orientation: 'landscape',
 	                title: 'DataClientes',
@@ -15,34 +26,43 @@ $(document).ready(function() {
 						     	}
 						 }
 		        },
-		        'excel',
-		        'colvis'
+		        {
+		        	text:'<i class="fa fa-file-excel-o" aria-hidden="true"></i>',
+		        	extend:	'excel',
+		        	titleAttr: 'Importar a Excel'
+		        },
+		        {
+		        	text:'Visualizar',
+		        	extend: 'colvis',
+		        	titleAttr: 'Visualizar columnas'
+		        }
+
 		    		],
-		    "language":idioma_espanol
+		    "language": idioma_espanol = {
+				    "sProcessing":     "Procesando...",
+				    "sLengthMenu":     "Mostrar _MENU_ registros",
+				    "sZeroRecords":    "No se encontraron resultados",
+				    "sEmptyTable":     "Ningún dato disponible en esta tabla",
+				    "sInfo":           "Mostrando del _START_ al _END_ de  _TOTAL_ clientes",
+				    "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+				    "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+				    "sInfoPostFix":    "",
+				    "sSearch":         "Buscar cliente:",
+				    "sUrl":            "",
+				    "sInfoThousands":  ",",
+				    "sLoadingRecords": "Cargando...",
+				    "oPaginate": {
+				        "sFirst":    "Primero",
+				        "sLast":     "Último",
+				        "sNext":     "Siguiente",
+				        "sPrevious": "Anterior"
+				    },
+				    "oAria": {
+				        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+				        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+				}
+			}
 		});
 	});
 
-var idioma_espanol = {
-	    "sProcessing":     "Procesando...",
-	    "sLengthMenu":     "Mostrar _MENU_ registros",
-	    "sZeroRecords":    "No se encontraron resultados",
-	    "sEmptyTable":     "Ningún dato disponible en esta tabla",
-	    "sInfo":           "Mostrando del _START_ al _END_ de  _TOTAL_ clientes",
-	    "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
-	    "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
-	    "sInfoPostFix":    "",
-	    "sSearch":         "Buscar cliente:",
-	    "sUrl":            "",
-	    "sInfoThousands":  ",",
-	    "sLoadingRecords": "Cargando...",
-	    "oPaginate": {
-	        "sFirst":    "Primero",
-	        "sLast":     "Último",
-	        "sNext":     "Siguiente",
-	        "sPrevious": "Anterior"
-	    },
-	    "oAria": {
-	        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-	        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-	}
-};
+
