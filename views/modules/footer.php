5e5143b299a5c9af0554346f5aa2245592b96b7c
Juanganzo69 <juan1910@live.com.mx>
2017-04-23 15:14:47 -0500

    footer
    Cambio de los styles del footer al css principal

--- views/modules/footer.php
+++ views/modules/footer.php
@@ -1,16 +1,3 @@
-<style>
-	.footer{
-	    position: absolute;
-    	bottom: -30x;
-   		width: 100%;
-	}
-
-
-
-
-
-</style>
-
 <footer class="footer bg-inverse text-center">
 <div class="container">
   <p class="text-white">
