a6d51357cf4b3e5435639aa169c1d24c6c918553
Juanganzo69 <juan1910@live.com.mx>
2017-05-09 20:08:40 -0500

    Nuevos links
    Nuevos archivos necesarios y eliminación de archivos innecesarios

--- views/template.php
+++ views/template.php
@@ -12,7 +12,7 @@
 	<link rel="stylesheet" href="views/assets/css/css/dataTables.bootstrap4.min.css">
 	<link rel="stylesheet" href="views/assets/css/css/buttons.bootstrap4.min.css">
 	<link rel="stylesheet" href="views/assets/css/css/bootstrap.min.css">
-	
+
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 	<script type="text/javascript" src="views/assets/js/jquery-1.12.4.js"></script>
 	<script type="text/javascript" src="views/assets/js/jquery.dataTables.min.js"></script>
@@ -27,17 +27,16 @@
 	<script type="text/javascript" src="views/assets/js/buttons.html5.min.js"></script>
 	<script type="text/javascript" src="views/assets/js/buttons.print.min.js"></script>
 	<script type="text/javascript" src="views/assets/js/buttons.colVis.min.js"></script>
-	<script type="text/javascript" src="views/assets/js/tablaClientes.js"></script>
-
-	<script type="text/javascript" src="views/assets/js/bootstrap-datetimepicker.js"></script>
-	<link rel="stylesheet" type="text/css" href="views/assets/css/bootstrap-datetimepicker.css">
+	<script type="text/javascript" src="views/assets/js/Tabla.js"></script>
+	<script type="text/javascript" src="views/assets/js/Advertencia.js"></script>
 
 
 
 
 
-    
-   
+
+
+
 </head>
 
 <body>
@@ -46,18 +45,13 @@
 
 <section>
 
-<?php 
+<?php
 
 $mvc = new ControllerAdministrador();
 $mvc -> enlacesPaginasController();
 
  ?>
 </section>
-<!-- <script type="text/javascript" src="views/assets/jquery/jquery-1.8.3.min.js" charset="UTF-8"></script>
- --><!-- <script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script> -->
-<!-- <script type="text/javascript" src="../js/bootstrap-datetimepicker.js" charset="UTF-8"></script> -->
-<script type="text/javascript" src="views/assets/js/locales/bootstrap-datetimepicker.fr.js" charset="UTF-8"></script>
-
 </body>
 
 </html>
