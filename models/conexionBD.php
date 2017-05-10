871b0c1b2bd7ddd49b204d7aa861aeedd3849b84
Juanganzo69 <juan1910@live.com.mx>
2017-05-09 20:09:29 -0500

    Mi root

--- models/conexionBD.php
+++ models/conexionBD.php
@@ -4,7 +4,7 @@ class Conexion{
 
 	public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=solucionesid","root","ger3uicab");
+		$link = new PDO("mysql:host=localhost;dbname=solucionesid","root","Watusi04");
 		return $link;
 
 	}
